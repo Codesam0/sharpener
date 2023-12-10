b16c40beaace5d008571b832c463d602e4c4fa09
Codesam0 <samsraj2000@gmail.com>
2023-12-10 21:14:10 +0530

    assignment - 3

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-console.log("commit id")
+doc1 = document.getElementById("main-header")
+doc1.style.borderBottom = " solid 3px black"
+doc2 = document.getElementsByClassName("title")
+doc2[0].style.color = " red "
