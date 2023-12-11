f474ebf42685c8e55c56a36d2ce10d1f3de6bd98
Codesam0 <samsraj2000@gmail.com>
2023-12-11 14:46:06 +0530

    query and queryall assignment

--- test.js
+++ test.js
@@ -4,9 +4,19 @@
 // newli[4].style.fontWeight = " bold "
 // secondele = document.querySelector('.list-group-item:nth-child(2)');
 // secondele.style.color = "red"
-secondele = document.querySelector('.list-group-item:nth-child(3)');
-console.log(secondele)
-secondele.style.color = "red";
-secondele = document.querySelector('.list-group-item:nth-child(3)');
-console.log(secondele)
-secondele.style.color = "white";
+// Using QuerySelectorALL change the font color to green for 2nd item in the item list
+// Choose all the odd elements and make their background green using QuerySelectorALL﻿
+
+// secondele = document.querySelector('.list-group-item:nth-child(3)');
+// console.log(secondele)
+// secondele.style.color = "red";
+
+// secondele = document.querySelector('.list-group-item:nth-child(3)');
+// console.log(secondele)
+// secondele.style.color = "white";
+secquery = document.querySelectorAll(".list-group-item")
+secquery[1].style.color = "green"
+odd = document.querySelectorAll(".list-group-item:nth-child(odd")
+for(i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = "green"
+}
