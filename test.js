4cd04839626194d45c59325fb70a0b4b01fc2581
Codesam0 <samsraj2000@gmail.com>
2023-12-11 14:31:29 +0530

    get

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
-doc1 = document.getElementById("main-header")
-doc1.style.borderBottom = " solid 3px black "
-doc2 = document.getElementsByClassName(" title ")
-doc2[0].style.color = " orange "
-doc3 = document.getElementsByClassName("list-group-item")
-doc3[1].style.backgroundColor = "red"
-for ( i=0 ; i<doc3.length ; i++){
-    doc3[i].style.fontWeight = "bold"
-}
-doc4 = document.getElementById("items")
-li = document.createElement("li")
-li.textContent = " item 5 "
-doc4.append(li)
-newli = document.getElementsByTagName("li")
-newli[4].style.fontWeight = "bold"
+// doc1 = document.getElementById("main-header")
+// doc1.style.borderBottom = " solid 3px black "
+// newli = document.getElementsByTagName(" li ")
+// newli[4].style.fontWeight = " bold "
+// secondele = document.querySelector('.list-group-item:nth-child(2)');
+// secondele.style.color = "red"
+secondele = document.querySelector('.list-group-item:nth-child(3)');
+console.log(secondele)
+secondele.style.color = "red";
+secondele = document.querySelector('.list-group-item:nth-child(3)');
+console.log(secondele)
+secondele.style.color = "white";
