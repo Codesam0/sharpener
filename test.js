63401a8cb9e90ae44f1177d00615a6fe7c4bcad8
Codesam0 <samsraj2000@gmail.com>
2023-12-11 13:15:36 +0530

    changed into bold list and green background

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
 doc1 = document.getElementById("main-header")
-doc1.style.borderBottom = " solid 3px black"
-doc2 = document.getElementsByClassName("title")
-doc2[0].style.color = " red "
+doc1.style.borderBottom = " solid 3px black "
+doc2 = document.getElementsByClassName(" title ")
+doc2[0].style.color = " orange "
+doc3 = document.getElementsByClassName("list-group-item")
+doc3[1].style.backgroundColor = "red"
+for ( i=0 ; i<doc3.length ; i++){
+    doc3[i].style.fontWeight = "bold"
+}
